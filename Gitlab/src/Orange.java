b8625abf01f1ea49ef2b11c22220bb22c941974e
User <User@192.168.0.202>
2023-02-22 19:12:00 +0530

    this is my commit branch

--- Gitlab/src/Orange.java
+++ Gitlab/src/Orange.java
@@ -6,11 +6,16 @@ public class Orange {
 		{
 		this.color=color;
 		}
+		String getColor()
+		{
+		return color;
+		}
 		public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Orange greenApple = new Orange("green");
 		String color = greenApple.getColor();
 		System.out.println( "color of apple is " + color);
+		
 
 		}
 }
